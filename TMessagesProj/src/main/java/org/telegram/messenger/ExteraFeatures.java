ed634d4dfc3d5b9bbc7e45693a7c072c346095af
TEAM-WAD <92700528+TEAM-WAD@users.noreply.github.com>
2026-08-28 05:14:23 +0300

    Update ExteraFeatures.java

--- TMessagesProj/src/main/java/org/telegram/messenger/ExteraFeatures.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ExteraFeatures.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.telegram.tgnet.ConnectionsManager;
+import org.telegram.tgnet.TLRPC;
 
 /** Local-only Extera settings/state. */
 public final class ExteraFeatures {
